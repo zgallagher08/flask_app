c34ddfac2e55d8d5f782ccec8fed9dac70cb1c6e
Zach Gallagher <zachagallagher@gmail.com>
2018-08-11 17:21:01 -0700

    Added articles page and article template

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+  articles = [
+    {
+      'id': 1,
+      'title': 'Article One',
+      'body': 'This is the first article. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, debitis quam cupiditate facilis harum tempora sit ratione consequuntur?',
+      'author': 'Zach Gallagher',
+      'create_date': '08-11-2018'
+    },
+    {
+      'id': 2,
+      'title': 'Article Two',
+      'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam molestias quod, consectetur facilis. Ea quam quaerat eius fugit, est, dolorem.',
+      'author': 'Kanye West',
+      'create_date': '08-11-2018'
+    },
+    {
+      'id': 3,
+      'title': 'Article Three',
+      'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, rerum.',
+      'author': 'Kendrick Lamar',
+      'create_date': '08-11-2018'
+    }
+  ]
+  return articles
